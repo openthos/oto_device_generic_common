2d92b3a8cc47e79e88347650dfd3e4fe39598da4
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-04-27 18:35:52 +0800

    init.sh: handle amdgpudrmfb by gralloc.drm

--- init.sh
+++ init.sh
@@ -116,7 +116,7 @@ function init_uvesafb()
 function init_hal_gralloc()
 {
 	case "$(cat /proc/fb | head -1)" in
-		0*inteldrmfb|0*radeondrmfb|0*nouveaufb)
+		0*inteldrmfb|0*radeondrmfb|0*nouveaufb|0*amdgpudrmfb)
 			set_property ro.hardware.gralloc drm
 			set_drm_mode
 			;;
