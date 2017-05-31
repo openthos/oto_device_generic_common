2036465312772a0b4c753b91b3929166815751d1
Chen Wei <chenwei01@thtfpc.com>
2017-05-31 08:13:23 +0000

    Include overlayfs makefile
    Signed-off-by: Chen Wei <chenwei01@thtfpc.com>
    Signed-off-by: Jianxing.Wang <wangjianxing5210@163.com>

--- device.mk
+++ device.mk
@@ -108,3 +108,6 @@ $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 
 # Get native bridge settings
 $(call inherit-product-if-exists,$(LOCAL_PATH)/nativebridge/nativebridge.mk)
+
+# Get overlayfs settings
+$(call inherit-product-if-exists,vendor/openthos/overlayfs.mk)
