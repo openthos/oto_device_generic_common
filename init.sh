38d07fc77f07fd8a86afbf45d938a547380d7c74
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-01-12 11:08:54 +0800

    init.sh: adjust mic boost to 0

--- init.sh
+++ init.sh
@@ -373,8 +373,8 @@ function do_bootcomplete()
 			alsa_amixer -c $c set Capture cap
 			alsa_amixer -c $c set PCM 100 unmute
 			alsa_amixer -c $c set SPO unmute
-			alsa_amixer -c $c set 'Mic Boost' 3
-			alsa_amixer -c $c set 'Internal Mic Boost' 3
+			alsa_amixer -c $c set 'Mic Boost' 0
+			alsa_amixer -c $c set 'Internal Mic Boost' 0
 		fi
 	done
 
