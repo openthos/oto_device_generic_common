71a9ae0028cbe7cf4a3046787b30e35ded915fa2
elliott10 <xiaoluoyuan@163.com>
2018-11-27 12:13:11 +0000

    Save the seafile log file

--- init.sh
+++ init.sh
@@ -382,7 +382,7 @@ function do_bootcomplete()
 	pm disable com.android.browser
 	[ "`getprop ro.build.type`" = "user" ] && pm path com.android.vending | grep -q /system && sleep 5 && pm disable com.android.vending
 
-	/system/bin/sh /system/linux/sea/keeper/seaf_start.sh &
+	/system/bin/sh /system/linux/sea/keeper/seaf_start.sh >> /cache/keeper.log 2>&1 &
 }
 
 function do_hci()
